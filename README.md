24a02b6cad3d1b7eba9064c992f650a776f42473
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-21 16:39:12 -0600

    made test branch and worked on said branch

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jq_rps
+
+// Create your html, css, and javascript file inside a folder
+// Create a git repository for your project and link it to a new GitHub repository.
+// Add jQuery to your project
+// User selects rock, paper, or scissors
+// Computer randomly chooses (hint: have your choices in an array and randomly choose) 
+// DISPLAY the user and computers choice on the screen
+// DISPLAY the winner.
--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-// Create your html, css, and javascript file inside a folder
-// Create a git repository for your project and link it to a new GitHub repository.
-// Add jQuery to your project
 // User selects rock, paper, or scissors
 // Computer randomly chooses (hint: have your choices in an array and randomly choose) 
 // DISPLAY the user and computers choice on the screen
 // DISPLAY the winner
 
-(document).ready(function() {
 
-});
+
+$(document).ready(function() { 
+  
+  var choices = ['Rock', 'Paper', 'Scissors']
+  var playerChoice 
+  var computerChoice
+
+  function computerChoice() {
+    choices.each[Math.floor(Math.random() * choices.length)]
+  }
+
+
+  $('#rock').on('click', function(){
+    playerChoice = 'rock';
+    computerChoice();
+    result();
+   });
+
+  $('#paper').on('click', function(){
+    playerChoice = 'paper';
+    computerChoice();
+    result();
+   });
+
+  $('#scissors').on('click', function(){
+    playerChoice = 'scissors';
+    computerChoice();
+    result();
+  })
+
+
+  
+
+})
